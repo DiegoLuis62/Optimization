044299570c0741b565da4316479efb38eade1736
Almor21 <98895136+Almor21@users.noreply.github.com>
2024-05-06 23:34:03 -0500

    Se agrego el modelo 2 y se organizo la pagina de models

--- js/web.js
+++
@@ -1,9 +0,0 @@
-function showSelect(nm) {
-    const slc = document.getElementById("span_op" + nm)
-    slc.style.display = "inline-block"
-}
-
-function noShowSelect(nm) {
-    const slc = document.getElementById("span_op" + nm)
-    slc.style.display = "none"
-}
